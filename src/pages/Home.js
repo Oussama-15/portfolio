6d9bd8f8e0e59ed7c05a295de2a3f23ed400b3c2
Oussama Baccar <oussamabaccar4@gmail.com>
2022-08-21 14:30:34 +0100

    Add title

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,7 +5,8 @@ function Home() {
   return (
     <div className="alert alert-info mt-5 container" role="alert">
       <img className ="with-border-radius" src="/images/photo-1.jpg" width="150px" alt="photo" />
-      <p>Hello my name is Oussama, I'am a Fullstack Web Developper</p>
+      <h1>Oussama Baccar</h1>
+      <p>Hello my name is Oussama Baccar, I'am a Fullstack Web Developper</p>
       <img src={webImage} style={{width: "100%"}} alt="Web"/>
     </div> 
   )
